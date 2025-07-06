8912e17d2308219db6df717bea5a5c9b6bd7b1af
I-am-Youjin <atupenko@gmail.com>
2025-07-06 22:10:49 +0300

    add html changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/shri-performance/",
   plugins: [react()],
+  build: {
+    assetsDir: "assets",
+    manifest: true,
+  },
 });
